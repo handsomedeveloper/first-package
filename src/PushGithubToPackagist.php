bb9750f93e308c603853e847eebe56858ab47456
Jie She <handsomedeveloperjie@gmail.com>
2019-01-27 03:30:08 +0800

    Push code to Packagist from Github

---
+++ src/PushGithubToPackagist.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SheJie
+ * Date: 2019-01-27
+ * Time: 03:27
+ */
+
+namespace HandsomeDeveloper\FirstPackage;
+
+
+class PushGithubToPackagist
+{
+    public static function echo()
+    {
+        echo 'Push the code to Packagist from Github.';
+    }
+}
