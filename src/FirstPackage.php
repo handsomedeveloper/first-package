369304c412a1ab8e67babaf39c5c7df6f5e97eab
Jie She <handsomedeveloperjie@gmail.com>
2019-01-27 02:56:39 +0800

    init && dev package

---
+++ src/FirstPackage.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SheJie
+ * Date: 2019-01-27
+ * Time: 01:40
+ */
+
+namespace HandsomeDeveloper\FirstPackage;
+
+
+class FirstPackage
+{
+    public function desc()
+    {
+        echo 'This is my first package.';
+    }
+
+}
